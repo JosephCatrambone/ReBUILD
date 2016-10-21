ceeeff1d3c844e311d55219ec54653bf872b9a1c
Joseph Catrambone <jo.jcat@gmail.com>
2016-10-20 23:28:29 -0700

    Fix some build problems.  Move over code from another project.

---
+++ src/geometry.rs
@@ -0,0 +1,5 @@
+
+#[derive(Copy, Clone)]
+pub struct Vertex2f {
+	position: [f32; 2],
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
 #[macro_use]
 #[allow(dead_code)]
+#[allow(unused_imports)]
 
 extern crate rand;
 extern crate glium;
 
 mod geometry;
 
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 use glium::{DisplayBuild, Surface};
 use glium::index::PrimitiveType;
 use glium::glutin;
 
-implement_vertex!(Vertex2f, position);
+use geometry::Vertex2f;
 
 fn main() {
 	const WINDOW_WIDTH: usize = 1280;
 	const WINDOW_HEIGHT: usize = 1024;
 
 	let mut display = glutin::WindowBuilder::new().build_glium().unwrap();
-	let mut timeAccumulator = Duration::new(0, 0);
-	let mut now = Instant::now();
+	//let mut timeAccumulator = Duration::new(0, 0);
+	//let mut now = Instant::now();
+
+	let frame_delay = Duration::from_millis(16u64);
+	let vertex_shader_src = include_str!("vert_shader.cl");
+	let fragment_shader_src = include_str!("frag_shader.cl");
+	let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
 	'main: loop {
+		// Start stopwatch.
+		let draw_start = Instant::now();
+
 		// Draw loop.
 		let mut target = display.draw();
 		target.clear_color(1.0, 0.0, 1.0, 1.0);
-		target.finish.unwrap();
+		target.finish().unwrap();
 
 		// Event loop.
 		for event in display.poll_events() {
@@ -37,5 +47,14 @@ fn main() {
 		}
 
 		// Logic loop.
+
+		// Delay.
+		// Frame finish time.
+		let delta_time = draw_start.elapsed();
+
+		// Lock to 60fps.
+		if delta_time < frame_delay {
+			thread::sleep(frame_delay - delta_time);
+		}
 	}
 }
